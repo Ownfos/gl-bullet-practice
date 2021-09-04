720b020770ba11f9b7269146a65290380801a8b7
Ownfos <39623255+Ownfos@users.noreply.github.com>
2021-09-05 00:14:38 +0900

    Implement dragging objects

--- gl-bullet-practice/sources/main.cpp
+++ gl-bullet-practice/sources/main.cpp
@@ -71,7 +71,7 @@ int main()
                 .position = {0, -11, 0},
                 .scale = {10, 0.2, 10}
             }
-        });
+            });
         world.add_rigid_body(ground);
 
         // Place a mini cube above the ground.
@@ -83,8 +83,24 @@ int main()
                 .rotation = {{0, 0, 1}, glm::radians(45.1f)}, // Rotate 45.1 degrees around +Z axis
                 .scale = {1, 0.5, 0.5}
             }
-        });
+            });
         world.add_rigid_body(cube_object);
+
+        std::vector<std::shared_ptr<RigidBody>> clones;
+        for (int pos = 0; pos < 5; ++pos)
+        {
+            auto cube_object2 = std::make_shared<RigidBody>(RigidBodyConfig{
+                .shape = std::make_shared<btBoxShape>(btVector3{ 1, 1, 1 }),
+                .mass = 1.0f,
+                .transform = {
+                    .position = {4.5f, pos * 2.0f, 5.0f},
+                    .rotation = {{0, 0, 1}, glm::radians(45.1f)}, // Rotate 45.1 degrees around +Z axis
+                    .scale = {1, 0.5, 0.5}
+                }
+                });
+            world.add_rigid_body(cube_object2);
+            clones.push_back(cube_object2);
+        }
 
         // Prepare camera for view and projection matrix
         auto cam_yaw = 0.0f;
@@ -104,6 +120,13 @@ int main()
         auto prev_cursor = window.get_normalized_cursor_pos();
         window.set_cursor_enabled(false);
 
+        // Variables used to drag an object around the camera with mouse cursor
+        float last_raycast_distance;
+        glm::vec4 clicked_point_local;
+        btVector3 point_of_impact;
+        btVector3 drag_target_point;
+        RigidBody* dragging_object = nullptr;
+
         window.register_key_handler([&](auto key, auto action) {
             // Cursor activation
             if (key == GLFW_KEY_LEFT_ALT && action == GLFW_PRESS) window.set_cursor_enabled(true);
@@ -113,6 +136,44 @@ int main()
             if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
             {
                 window.set_close_flag();
+            }
+        });
+
+        window.register_mouse_button_handler([&](auto button, auto action, auto mods) {
+            // Check if there's an object to drag with mouse
+            if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+            {
+                // Coordinate conversion from normalized screen space to world space
+                auto screen_pos = window.get_normalized_cursor_pos();
+                auto world_pos = camera.screen_to_world_point(screen_pos);
+
+                // Find the closest object
+                auto ray = Ray::from_screenpoint(camera, screen_pos);
+                auto result = world.raycast_closest(ray);
+
+                if (result.has_value())
+                {
+                    // This object will be referenced in the main loop to apply force
+                    dragging_object = result->object;
+
+                    // Since raycast results are given in world coordinate,
+                    // the values become invalid after the object moves.
+                    //
+                    // In order to keep track of the same point on the model,
+                    // convert the clicked point to the model's local coordinate.
+                    auto local_point = glm::inverse(result->object->get_world_transform_matrix()) * glm::vec4(bt2glm(result->position), 1.0f);
+                    clicked_point_local = local_point / local_point.w;
+
+                    // The object will now get dragged to a point in a sphere around camera position,
+                    // where the radius is the initial raycast distance.
+                    last_raycast_distance = ray.start_point.distance(result->position);
+                }
+            }
+
+            // Stop dragging object
+            if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+            {
+                dragging_object = nullptr;
             }
         });
 
@@ -154,42 +215,37 @@ int main()
                     prev_cursor = current_cursor;
                 }
 
-                // Test screen to world coordinate conversion, raycast, and force application
+                // Drag clicked object
                 {
-                    // Coordinate conversion from normalized screen space to world space
-                    auto screen_pos = window.get_normalized_cursor_pos();
-                    auto world_pos = camera.screen_to_world_point(screen_pos);
-
-                    // Find the closest object
-                    auto ray = Ray::from_screenpoint(camera, screen_pos);
-                    auto result = world.raycast_closest(ray);
-
-                    if (result.has_value())
+                    if (dragging_object != nullptr && window.get_mouse_button_state(GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
                     {
-                        std::cout << fmt::format(
-                            "[Raycast Hit Closest] world position: {} world normal: {}\n",
-                            result->position,
-                            result->normal
-                        );
+                        // Calculate the position in world space where we want the clicked point to reach
+                        auto screen_pos = window.get_normalized_cursor_pos();
+                        auto ray = Ray::from_screenpoint(camera, screen_pos);
+                        drag_target_point = ray.start_point + ray.get_direction() * last_raycast_distance;
 
-                        // Pull object
-                        if (window.get_mouse_button_state(GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+                        // Calculate the world space coordinate of the clicked point
+                        auto clicked_point_world = dragging_object->get_world_transform_matrix() * clicked_point_local;
+                        point_of_impact = glm2bt(glm::vec3(clicked_point_world / clicked_point_world.w));
+
+                        // Only apply force when distance is big enough.
+                        // This prevents divide by 0 happening when we calculate force direction.
+                        if (drag_target_point.distance2(point_of_impact) > 0.001f)
                         {
-                            // Apply a force parallel to the normal vector of the selected point
-                            auto force_point = result->position - result->object->get_transform().position;
-                            auto force = result->normal * 10.0f;
+                            // The relative coordinate of the point in world space where we want to apply force
+                            auto local_offset = point_of_impact - dragging_object->get_world_transform().getOrigin();
 
-                            result->object->apply_force(force, force_point);
-                        }
+                            // A spring-like force towards the target point
+                            auto force = (drag_target_point - point_of_impact).normalized() * 20.0f;
 
-                        // Push object
-                        if (window.get_mouse_button_state(GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS)
-                        {
-                            // Apply a force parallel to the ray direction
-                            auto force_point = result->position - result->object->get_transform().position;
-                            auto force = ray.get_direction() * 10.0f;
+                            dragging_object->apply_force(force, local_offset);
 
-                            result->object->apply_force(force, force_point);
+                            std::cout << fmt::format(
+                                "[Draging] drag target: {} -> target point: {}, local offset: {}\n",
+                                point_of_impact,
+                                drag_target_point,
+                                local_offset
+                            );
                         }
                     }
                 }
@@ -214,10 +270,43 @@ int main()
                 shader.set_uniform("color", glm::vec4{ 0.5f, 0.7f, 0.9f, 1.0f });
                 draw_indexed(GL_TRIANGLES, 36, 0);
 
+                // Draw the falling mini cube
+                shader.set_uniform("color", glm::vec4{ 0.7f, 0.2f, 0.5f, 1.0f });
+                for (const auto& cube : clones)
+                {
+                    shader.set_uniform("world", cube->get_world_transform_matrix());
+                    draw_indexed(GL_TRIANGLES, 36, 0);
+                }
+
                 // Draw the ground
                 shader.set_uniform("world", ground->get_world_transform_matrix());
                 shader.set_uniform("color", glm::vec4{ 1.0f, 0.0f, 0.0f, 0.7f });
                 draw_indexed(GL_TRIANGLES, 36, 0);
+
+
+                // Draw gizmos for object drag positions (dragging point on object & target point to move)
+                if (dragging_object != nullptr)
+                {
+                    {
+                        glm::mat4 transform(1.0f);
+                        transform = glm::translate(transform, bt2glm(point_of_impact));
+                        transform = glm::scale(transform, { 0.1, 0.1, 0.1 });
+
+                        shader.set_uniform("world", transform);
+                        shader.set_uniform("color", glm::vec4{ 0.0f, 1.0f, 0.0f, 1.0f });
+                        draw_indexed(GL_TRIANGLES, 36, 0);
+                    }
+
+                    {
+                        glm::mat4 transform(1.0f);
+                        transform = glm::translate(transform, bt2glm(drag_target_point));
+                        transform = glm::scale(transform, { 0.1, 0.1, 0.1 });
+
+                        shader.set_uniform("world", transform);
+                        shader.set_uniform("color", glm::vec4{ 1.0f, 0.0f, 1.0f, 1.0f });
+                        draw_indexed(GL_TRIANGLES, 36, 0);
+                    }
+                }
 
                 window.swap_buffer();
             }
